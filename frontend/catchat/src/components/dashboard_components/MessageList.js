b142a2c44191d9af76790c4e2ab93f163db8d016
Kaushal Patel <patelkaushalb1@gmail.com>
2023-12-13 08:10:44 -0500

    Fixed messaging. Now works as intended. Small bug of autoscroll.

--- frontend/catchat/src/components/dashboard_components/MessageList.js
+++ frontend/catchat/src/components/dashboard_components/MessageList.js
@@ -17,12 +17,39 @@ function MessageList({ sender }) {
   // State to store the user id of the user that the logged in user is chatting with
   const[userId, setUserId] = useState("");
 
+  // Reference to the end of the message list
+  const messageEndRef = React.useRef(null);
+
   // On every render, do the following
   useEffect(() => {
     // Listening for our EventEmitter events
     eventEmitter.on("cardClicked", cardClickListener);
     eventEmitter.on('messageSent', messageListener);
+
+    return () => {
+        // Remove the event listeners
+        eventEmitter.unsubscribe("cardClicked", cardClickListener);
+        eventEmitter.unsubscribe('messageSent', messageListener);
+
+        // Remove the socket listeners
+        socket.off("messageHistory");
+        socket.off("newMessage");
+    }
   });
+
+  // On component mount and when the messages state changes, do the following
+  useEffect(() => {
+    if (messages.length === 0) {
+        console.log("Messages state cleared, now fetching message history.");
+        getMessageHistory(sender, userId);
+    }
+  }, [messages]);
+
+  // Scroll to the bottom of the message list when the messages state changes
+    useEffect(() => {
+      messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
+
 
   /**
   //    * Callback function that is called when the 'cardClicked' event is emitted
@@ -57,8 +84,15 @@ function MessageList({ sender }) {
         console.log("Message sent: " + message);
         console.log("To: " + userId);
         console.log("From: " + sender);
+
         // Send the message to the server via the socket connection passing in the sender, message, and userId
         socket.emit("newMessage", { sender, message, userId });
+
+        // Clear the message state
+        setMessages([]);
+
+        console.log("Clearing messages")
+        console.log(messages);
     }
 
   /**
@@ -67,7 +101,8 @@ function MessageList({ sender }) {
    * @param receiver The username of the user that the logged in user is chatting with
    */
   const getMessageHistory = (sender, receiver) => {
-    //console.log("Getting message history between " + sender + " and " + receiver);
+    // Remove the previous listener
+    socket.off("messageHistory");
 
     // Send the server a request for the message history between the logged in user and the user that they are chatting with
     socket.emit("getMessageHistory", { sender, receiver });
@@ -83,20 +118,27 @@ function MessageList({ sender }) {
   };
 
   return (
-      <div className="flex items-center justify-center h-full">
-        <div className="p-4">
+      <div className="flex h-[600px] w-full overflow-y-scroll">
+        <div className="p-4 w-full">
           {messages.length > 0 ? (
-            <ul>
-              {messages.map((message, index) => (
-                <li key={index}>From: {message.from_user_id} - {message.message}</li>
-              ))}
-            </ul>
-          ) : (
-            <p className={"font-bold"}>Click on a chat to load messages</p>
-          )}
-        </div>
-      </div>
-    );
+        <ul>
+          {messages.map((message, index) => (
+            <li key={index} className="my-2">
+              <div className={` ${message.from_user_id === sender ? 'text-right' : 'text-left'}`}>
+                <div>From: {message.from_user_id}</div>
+                <div>{message.message}</div>
+              </div>
+            </li>
+          ))}
+          {/* Reference for end of message list */}
+          <div ref={messageEndRef} />
+        </ul>
+      ) : (
+        <p className="font-bold text-center">Click on a chat to load messages</p>
+      )}
+    </div>
+  </div>
+);
 
 
 }
