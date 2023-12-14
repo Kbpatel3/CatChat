767f5d755011036c1efe38e8dc42a7025e09cf3a
Kaushal Patel <patelkaushalb1@gmail.com>
2023-12-14 15:46:34 -0500

    Added stream cipher for javascript

---
+++ frontend/catchat/src/stream_cipher/StreamCipher.js
@@ -0,0 +1,50 @@
+class StreamCipher {
+    constructor(keyString) {
+        const encoder = new TextEncoder();
+        this.key = encoder.encode(keyString); // Convert string key to byte array
+    }
+
+    generateKeystream(length) {
+        let keystream = new Uint8Array(length);
+        for (let i = 0; i < length; i++) {
+            keystream[i] = this.key[i % this.key.length];
+        }
+        return keystream;
+    }
+
+    encrypt(plaintext) {
+        const encoder = new TextEncoder();
+        const plaintextBytes = encoder.encode(plaintext);
+        const keystream = this.generateKeystream(plaintextBytes.length);
+        const ciphertext = new Uint8Array(plaintextBytes.length);
+        for (let i = 0; i < plaintextBytes.length; i++) {
+            ciphertext[i] = plaintextBytes[i] ^ keystream[i];
+        }
+        return ciphertext;
+    }
+
+    decrypt(ciphertext) {
+        const keystream = this.generateKeystream(ciphertext.length);
+        const plaintextBytes = new Uint8Array(ciphertext.length);
+        for (let i = 0; i < ciphertext.length; i++) {
+            plaintextBytes[i] = ciphertext[i] ^ keystream[i];
+        }
+        const decoder = new TextDecoder();
+        return decoder.decode(plaintextBytes);
+    }
+}
+
+function test() {
+    const key = "kdslfjlksjf";
+    const cipher = new StreamCipher(key);
+
+    const message = "Hello, world!";
+    const encryptedMessage = cipher.encrypt(message);
+    const decryptedMessage = cipher.decrypt(encryptedMessage);
+
+    console.log("Original message:", message);
+    console.log("Encrypted message:", encryptedMessage);
+    console.log("Decrypted message:", decryptedMessage);
+}
+
+test();
